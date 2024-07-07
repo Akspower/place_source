63b0067a0584f39ef10bc52bffe06d6f6122d05d
Akspower <adityakumarsharmapower@gmail.com>
2024-07-07 20:56:44 +0530

    insert

--- LLbasics.cpp
+++ LLbasics.cpp
@@ -19,7 +19,15 @@ Node(int data){
 }
 };
 
-
+int findlength(Node * &head){
+    int len=0;
+    Node*temp=head;
+    while(temp!=NULL){
+        temp=temp->next;
+        len++;
+    }
+    return len;
+}
 
 void insertathead(Node * &head,Node * &tail,int data){
 if(head==NULL){
@@ -47,6 +55,41 @@ void insertattail(Node * &head,Node * &tail,int data){
     tail=newnode;
 
 }
+void insertatposition(int position,int data,Node * &head,Node * &tail){
+        if(head==NULL){
+    Node* newnode= new Node(data);
+ head=newnode;
+ tail=newnode;
+ return;
+}
+if(position==0){
+    insertathead(head,tail,data);
+    return;
+}
+int len=findlength(head);
+if(position==len-1){
+    insertattail(head,tail,data);
+}
+
+//1,find pos
+   int i=1; 
+   Node *prev=head;
+   while (i<position)
+   {
+    prev=prev->next;
+    i++;
+   }
+   Node* curr=prev->next;
+
+   Node * newnode=new Node(data); //2
+   newnode->next=curr; //3
+   prev->next=newnode; //4
+   
+}
+
+
+
+
 void print(Node * &headhai){
     Node * temp=headhai; 
     while(temp!=NULL){
@@ -75,6 +118,9 @@ insertathead(head,tail,30);
 
 insertattail(head,tail,400);
 insertattail(head,tail,500);
+
+
+insertatposition(1,555,head,tail);
 
 print(head);
 }
