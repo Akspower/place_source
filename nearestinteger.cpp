fc23c308f1055a041566ce7c754124bebf0d2dfc
Akspower <adityakumarsharmapower@gmail.com>
2024-08-05 12:48:27 +0530

    nearest

---
+++ nearestinteger.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int nearestinteger(int num , int d){
+int r=num%d;
+if(r>(d+1)/2){
+    return num-r;
+}
+return num+(d-r);
+
+}
+
+int main(){
+    cout<<"enter num "<<endl;
+    int num,d;
+    cin>>num;
+    cout<<"enter divisor"<<endl;
+    cin>>d;
+    cout<<nearestinteger(num,d);
+
+}
