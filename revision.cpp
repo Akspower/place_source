ba59550a10b5d7d12b484a529cabf06a997e2f8c
Akspower <adityakumarsharmapower@gmail.com>
2024-08-07 14:42:50 +0530

    rev

--- revision.cpp
+++ revision.cpp
@@ -2,19 +2,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int buy(int * arr,int n){
-int buy=arr[0];
-int maxi=0;
-for(int i=0;i<n;i++){
-  if(buy>arr[i]){
-    buy=arr[i];
-  }
-    maxi=max(arr[i]-buy,maxi);
-  
-
-}
-  return maxi;
- 
+int fib(int n){
+  if(n==1) return n;
+  return n*fib(n-1);
 }
 
 
@@ -25,8 +15,7 @@ for(int i=0;i<n;i++){
 
 int main(){
  
-int arr[]={7,1,3,5,6,4};
-int n=sizeof(arr)/sizeof(arr[0]);
-int res=buy(arr,n);
+int n=5;
+int res=fib(n);
 cout<<res;
 }
