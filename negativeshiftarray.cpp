241e231ac6c373c0e579c9913f4c9652865e2e56
Akspower <adityakumarsharmapower@gmail.com>
2024-10-02 21:44:33 +0530

    change

--- negativeshiftarray.cpp
+++ negativeshiftarray.cpp
@@ -5,7 +5,7 @@ void shiftNegative(int arr[], int n) {
     int j = 0;
 
     for (int i = 0; i < n; i++) {
-        if (arr[i] <= 0) {
+        if (arr[i] >= 0) {
             swap(arr[i], arr[j]);
             j++;
         }
